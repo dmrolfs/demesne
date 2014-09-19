79886ef441e312026b8ba8442df0b173d9874ad5
Damon Rolfs <damon.rolfs@rightside.co>
2014-09-18 21:59:38 -0700

    refactor

--- src/main/scala/demesne/AggregateRootRef.scala
+++ src/main/scala/demesne/AggregateRootRef.scala
@@ -1,26 +1,18 @@
 package demesne
 
-import akka.actor.{ ActorContext, ActorLogging, ActorPath, ActorRef, ReceiveTimeout }
-import akka.persistence.{ EventsourcedProcessor, SnapshotOffer }
+import akka.actor.{ActorSystem, ActorContext, ActorPath, ActorRef}
 import peds.akka.envelope._
-import peds.commons.log.Trace
-import peds.commons.util._
 
 
 case class AggregateRootRef( rootType: AggregateRootType, id: Any, underlying: ActorRef ) extends Enveloping {
-  val trace = Trace[AggregateRootRef]
-  import AggregateRoot._
-
   def path: ActorPath = underlying.path
   override def pathname: String = path.name
 
-  def tell( message: Any, sender: ActorRef ): Unit = trace.block( s"tell(${message}, ${sender})" ) { 
-    underlying.send( message )( sender )
-  }
+  def tell( message: Any, sender: ActorRef ): Unit = underlying.send( message )( sender )
+  final def !( message: Any )( implicit sender: ActorRef = ActorRef.noSender ): Unit = tell( message, sender )
+  def forward( message: Any )( implicit context: ActorContext ): Unit = underlying sendForward message
+}
 
-  final def !( message: Any )( implicit sender: ActorRef = ActorRef.noSender ): Unit = trace.block( s"!(${message})(${sender})" ) { tell( message, sender ) }
-
-  def forward( message: Any )( implicit context: ActorContext ): Unit = trace.block( s"forward(${message})" ) {
-    underlying sendForward message
-  }
+object AggregateRootRef {
+  def deadLetters( implicit system: ActorSystem ): AggregateRootRef = AggregateRootRef( null, null, system.deadLetters )
 }
--- src/main/scala/demesne/package.scala
+++ src/main/scala/demesne/package.scala
@@ -1,3 +1,6 @@
 package object demesne {
+  val SystemKey: Symbol = 'system
+  val ModelKey: Symbol = 'model
+
   case object SaveSnapshot
 }
