e89ca67195912ca5dd2f61d214a14287b856202f
Damon Rolfs <damon.rolfs@rightside.co>
2014-09-28 21:13:09 -0700

    removed old commented-out code

--- src/main/scala/demesne/factory/package.scala
+++ src/main/scala/demesne/factory/package.scala
@@ -11,10 +11,6 @@ package object factory {
     system.actorOf( props, rootType.repositoryName )
   }
 
-//  def systemFactory( system: ActorSystem, rootType: AggregateRootType )( props: => Props ): ActorRef = {
-//    system.actorOf( props, rootType.repositoryName )
-//  }
-
   val clusteredFactory: ActorFactory = ( system: ActorSystem, rootType: AggregateRootType ) => ( props: Props ) => {
     val repoSpec = EnvelopingAggregateRootRepository specificationFor rootType
     ClusterSharding( system ).start(
@@ -33,22 +29,4 @@ package object factory {
 
     ClusterSharding( system ) shardRegion rootType.repositoryName
   }
-//  def clusteredFactory( system: ActorSystem, rootType: AggregateRootType )( props: => Props ): ActorRef = {
-//    val repoSpec = EnvelopingAggregateRootRepository specificationFor rootType
-//    ClusterSharding( system ).start(
-//      typeName = repoSpec.name,
-//      entryProps = Some( repoSpec.props ),
-//      idExtractor = repoSpec.idExtractor,
-//      shardResolver = repoSpec.shardResolver
-//    )
-//
-//    ClusterSharding( system ).start(
-//      typeName = rootType.name,
-//      entryProps = Some( rootType.aggregateRootProps ),
-//      idExtractor = rootType.aggregateIdFor,
-//      shardResolver = rootType.shardIdFor
-//    )
-//
-//    ClusterSharding( system ) shardRegion rootType.repositoryName
-//  }
 }
