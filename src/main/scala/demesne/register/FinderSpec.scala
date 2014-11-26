35276773f1aad492d80a13ac94c1add27e5d68a6
Damon Rolfs <drolfs@gmail.com>
2014-11-25 21:35:52 -0800

    added equality and hatched to play nice in registry lookup

--- src/main/scala/demesne/register/FinderSpec.scala
+++ src/main/scala/demesne/register/FinderSpec.scala
@@ -13,7 +13,7 @@ case class ContextChannelSubscription( channel: Class[_] ) extends RelaySubscrip
 case object RegisterBusSubscription extends RelaySubscription
 
 
-abstract class FinderSpec[K: ClassTag, I: ClassTag] {
+abstract class FinderSpec[K: ClassTag, I: ClassTag] extends Equals {
   def name: Symbol
   def keyIdExtractor: KeyIdExtractor[K, I]
   def agentProps( rootType: AggregateRootType ): Props
@@ -27,9 +27,34 @@ abstract class FinderSpec[K: ClassTag, I: ClassTag] {
   def relayProps( aggregatePath: ActorPath ): Props = RegisterRelay.props[K, I]( aggregatePath, keyIdExtractor )
   def relayClassifier( rootType: AggregateRootType ): String = rootType.name
 
+  override def hashCode: Int = {
+    41 * (
+      41 * (
+        41 + name.##
+      ) + key.##
+    ) + id.##
+  }
+
+  override def equals( rhs: Any ): Boolean = rhs match {
+    case that: FinderSpec[K, I] => {
+      if ( this eq that ) true
+      else {
+        ( that.## == this.## ) &&
+        ( that canEqual this ) &&
+        ( that.name == this.name ) &&
+        ( that.key == this.key ) &&
+        ( that.id == this.id )
+      }
+    }
+
+    case _ => false
+  }
+
+  override def canEqual( rhs: Any ): Boolean = rhs.isInstanceOf[FinderSpec[K, I]]
+
   override def toString: String = {
     val kname = implicitly[ClassTag[K]].runtimeClass.safeSimpleName
     val iname = implicitly[ClassTag[I]].runtimeClass.safeSimpleName
-    s"FinderSpec(${name.name}, ${kname}:${iname})"
+    s"FinderSpec[$kname, $iname](${name.name})"
   }
 }
