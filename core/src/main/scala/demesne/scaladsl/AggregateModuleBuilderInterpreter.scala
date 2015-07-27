8f9c85fd2fa981b5cdeeb8927e7466528cf5ade0
Damon Rolfs <drolfs@gmail.com>
2015-07-27 16:39:52 -0700

    separated ModuleBuilderInterpreter into own file

--- core/src/main/scala/demesne/scaladsl/AggregateModuleBuilderInterpreter.scala
+++ core/src/main/scala/demesne/scaladsl/AggregateModuleBuilderInterpreter.scala
@@ -1,54 +1,12 @@
 package demesne.scaladsl
 
 import scala.reflect.ClassTag
-import akka.actor.Actor
-import akka.actor.Actor.Receive
 import scalaz.{ Lens => _, _ }, Scalaz._
-import shapeless.{ Id => _, _ }
-import peds.commons.V
+import shapeless.Lens
 import peds.commons.log.Trace
 import peds.commons.util._
-import demesne.{ AggregateRoot, AggregateRootModule }
-import demesne.module.{ AggregateRootProps, DemesneModuleError, SimpleAggregateModule }
-import demesne.register.AggregateIndexSpec
+import demesne.module.{ AggregateRootProps, DemesneModuleError }
 
-
-trait ModuleBuilderInterpreter[S] {
-  import ModuleBuilderInterpreter._
-
-  def apply[A]( action: ModuleBuilderOp[A] ): Id[A]
-
-  def step[A]( action: ModuleBuilderOpF[A] ): Id[A] = {
-    val (result, m) = opStep( action )
-    module = m
-    result
-  }
-
-  def opStep[A]: PartialFunction[ModuleBuilderOpF[A], (Id[A], BuilderModule)]
-
-  def module: BuilderModule
-  def module_=( newModule: BuilderModule ): Unit
-
-  trait BuilderModule extends SimpleAggregateModule[S] {
-    def idTagO: Option[Symbol]
-    def idTagOLens: Lens[BuilderModule, Option[Symbol]]
-
-    def propsO: Option[AggregateRootProps]
-    def propsOLens: Lens[BuilderModule, Option[AggregateRootProps]]
-
-    // override def evState: ClassTag[S] = implicitly[ClassTag[S]]
-    override def aggregateIdTag: Symbol = idTagO getOrElse { throw UndefinedAggregateIdTagError }
-    override def aggregateRootPropsOp: AggregateRootProps = propsO getOrElse { throw UndefinedAggregateRootPropsError }
-  }
-}
-
-object ModuleBuilderInterpreter {
-  final case object UndefinedAggregateIdTagError 
-  extends IllegalArgumentException( "aggregateIdTag must be defined for Module" ) with DemesneModuleError
-
-  final case object UndefinedAggregateRootPropsError
-  extends IllegalArgumentException( "aggregate root actor props must be specificed for Module" ) with DemesneModuleError
-}
 
 class AggregateModuleBuilderInterpreter[S: ClassTag]() extends ModuleBuilderInterpreter[S] {
   import ModuleBuilderInterpreter._
