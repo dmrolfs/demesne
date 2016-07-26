9b30c80faa5d85946ee1b7a04c036bce637bf3a2
Damon Rolfs <drolfs@gmail.com>
2016-07-26 13:06:16 -0700

    bump snapshot versions

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -4,7 +4,7 @@ import sbt._
 import spray.revolver.RevolverPlugin._
 
 object BuildSettings {
-  val VERSION = "1.1.2"
+  val VERSION = "1.1.3-SNAPSHOT"
 
   val defaultBuildSettings = Defaults.coreDefaultSettings ++ Format.settings ++ Revolver.settings ++
     Seq(
