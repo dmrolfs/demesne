6d30f3d12f6cd8e6bd904de1dc2a179924eb51bb
Damon Rolfs <drolfs@gmail.com>
2017-01-18 10:18:57 -0800

    addressed a late surfaced compile error

--- core/src/main/scala/demesne/index/StackableIndexBusPublisher.scala
+++ core/src/main/scala/demesne/index/StackableIndexBusPublisher.scala
@@ -1,11 +1,12 @@
 package demesne.index
 
+import akka.actor.{ Actor, ActorLogging }
 import demesne.{ AggregateRootType, DomainModel }
 import peds.akka.publish.{ EventPublisher, Publisher, silent }
 import peds.commons.util.Chain._
 
 
-trait StackableIndexBusPublisher extends EventPublisher { outer: DomainModel.Provider with AggregateRootType.Provider =>
+trait StackableIndexBusPublisher extends EventPublisher { outer: Actor with ActorLogging with DomainModel.Provider with AggregateRootType.Provider =>
   abstract override def publish: Publisher = {
     val bus = IndexBus.bus( model.indexBus, outer.rootType )( _: IndexSpecification )
     outer.rootType.indexes.filter( _.relaySubscription == IndexBusSubscription ).foldLeft( super.publish ){ _ +> bus(_ ) }
