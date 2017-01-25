b1f614e69b8a8b8fe19a5b5c73879939e075b266
Damon Rolfs <drolfs@gmail.com>
2017-01-25 13:38:34 -0800

    cleaned up comments

--- core/src/main/scala/demesne/AggregateRootType.scala
+++ core/src/main/scala/demesne/AggregateRootType.scala
@@ -7,7 +7,6 @@ import akka.cluster.sharding.ShardRegion
 import akka.cluster.sharding.ShardRegion.Passivate
 import com.typesafe.scalalogging.LazyLogging
 import demesne.index.IndexSpecification
-import shapeless.TypeCase
 import peds.akka.envelope.Envelope
 import peds.akka.publish.ReliablePublisher.ReliableMessage
 import peds.commons.identifier.{Identifying, TaggedID}
@@ -31,7 +30,6 @@ abstract class AggregateRootType extends Equals with LazyLogging {
   def repositoryProps( implicit model: DomainModel ): Props
 
   val identifying: Identifying[_]
-  val TaggedIdType: TypeCase[TaggedID[_]] = TypeCase[TaggedID[_]]
 
   //todo: separate envelope & reliable like Relay's fillExtractor
   def aggregateIdFor: ShardRegion.ExtractEntityId = {
@@ -42,14 +40,6 @@ abstract class AggregateRootType extends Equals with LazyLogging {
     case e @ Envelope( payload, _ ) if aggregateIdFor.isDefinedAt( payload ) => ( aggregateIdFor(payload)._1, e ) // want MatchError on payload if not found
     case r @ ReliableMessage( _, msg ) if aggregateIdFor.isDefinedAt( msg ) => ( aggregateIdFor(msg)._1, r )  // want MatchError on msg if not found
     case p @ Passivate( stop ) if aggregateIdFor.isDefinedAt( stop ) => ( aggregateIdFor(stop)._1, p )
-    // case stop @ PassivationSpecification.StopAggregateRoot( TaggedIdType(tid) ) => {
-    //   logger.debug( "tagged aggregateIdFor(stop) = [{}]", (tid.id.toString, stop) )
-    //   ( tid.id.toString, stop )
-    // }
-    // case stop @ PassivationSpecification.StopAggregateRoot( id ) => {
-    //   logger.debug( "untagged aggregateIdFor(stop) = [{}]", (id.toString, stop) )
-    //   ( id.toString, stop )
-    // }
   }
 
   /**
@@ -70,14 +60,6 @@ abstract class AggregateRootType extends Equals with LazyLogging {
     case Envelope( payload, _ ) => shardIdFor( payload )
     case ReliableMessage( _, msg ) => shardIdFor( msg )
     case Passivate( stop ) => shardIdFor( stop )
-    // case stop @ PassivationSpecification.StopAggregateRoot( TaggedIdType(tid) ) => {
-    //   logger.debug( "tagged shardIdFor(stop) = [{}]", ( math.abs( tid.id.## ) % numberOfShards ).toString )
-    //   ( math.abs( tid.id.## ) % numberOfShards ).toString
-    // }
-    // case stop @ PassivationSpecification.StopAggregateRoot( id ) => {
-    //   logger.debug( "untagged shardIdFor(stop) = [{}]", ( math.abs( id.## ) % numberOfShards ).toString )
-    //   ( math.abs( id.## ) % numberOfShards ).toString
-    // }
   }
 
   /**
@@ -120,7 +102,7 @@ abstract class AggregateRootType extends Equals with LazyLogging {
     }
   }
 
-  override def hashCode(): Int = 41 * ( 41 + name.## ) + identifying.##
+  override val hashCode: Int = 41 * ( 41 + name.## ) + identifying.##
 
   override def toString: String = name + "AggregateRootType"
 }
