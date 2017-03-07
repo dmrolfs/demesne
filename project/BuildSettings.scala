c3097862e58abd15d55bfab3e2145779c1617888
Damon Rolfs <drolfs@gmail.com>
2017-03-06 17:40:34 -0800

    release versions

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -4,7 +4,7 @@ import sbt._
 import spray.revolver.RevolverPlugin._
 
 object BuildSettings {
-  val VERSION = "2.2.0-SNAPSHOT"
+  val VERSION = "2.2.2"
 
   val defaultBuildSettings = Defaults.coreDefaultSettings ++ Format.settings ++ Revolver.settings ++
     Seq(
